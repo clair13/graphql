388bdbc9dc18372e8f43556667c1f687691d5710
clair13 <theclairvoyant1313@yahoo.gr>
2018-08-16 21:40:25 +0300

    Implement error handling for createLink resolver

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user, optional: true
   has_many :votes
+
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :deacription, presence: true, length: { minimum: 5 }
 end
